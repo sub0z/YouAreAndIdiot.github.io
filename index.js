a5aecbad60f957f6c178870582f2347931d7505c
Sam <BESTVULCANCLIENT@gmail.com>
2022-06-09 20:53:34 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+ function newcreate() {
+window.open(
+      "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      "_blank",
+      "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400"
+    );
+    }
+    if (window.open) {
+    newcreate()
+    newcreate()
+    newcreate()
+    }
